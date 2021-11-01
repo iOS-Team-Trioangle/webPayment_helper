1df1deb9dbb932f59a9d14cfb763da7ea3b30703
suruthi <suruthi@gmail.com>
2021-11-01 13:38:45 +0530

    initial commit

---
+++ webPaymentHelper/webPaymentHelper.h
@@ -0,0 +1,18 @@
+//
+//  webPaymentHelper.h
+//  webPaymentHelper
+//
+//  Created by trioangle on 01/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for webPaymentHelper.
+FOUNDATION_EXPORT double webPaymentHelperVersionNumber;
+
+//! Project version string for webPaymentHelper.
+FOUNDATION_EXPORT const unsigned char webPaymentHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <webPaymentHelper/PublicHeader.h>
+
+
